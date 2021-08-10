cb39d66e84cb3dbdb2cae45ca51377bd0b10db20
jessalfredsen <jessalfredsen@gmail.com>
2021-08-10 14:20:46 +0200

    Managing find_packages.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="faktum_ai_tools",
+    name="faktum-ai-tools",
     version="0.0.2",
     author="faktum-ai",
     author_email="henrik@dataops.dk",
     description="Faktum AI Util",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Faktum-ai/faktum_ai_tools/",
+    url="https://github.com/Faktum-ai/faktum-ai-tools/",
     project_urls={
-        "Bug Tracker": "https://github.com/Faktum-ai/faktum_ai_tools/issues",
+        "Bug Tracker": "https://github.com/Faktum-ai/faktum-ai-tools/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
